c0dcb618c509b94bfa7c32723b0e17970f3f2896
hagglundoliver22 <55192953+hagglundoliver22@users.noreply.github.com>
2020-02-04 11:07:14 +0100

    yery

--- onepage/javascaript/onepage.js
+++ onepage/javascaript/onepage.js
@@ -1,10 +1,31 @@
 
-*$("#button").click(function(){
+
+$("#button").click(function(){
 
 
   $('html, body').animate({
-       scrollTop: $("#button").offset().top
-   }, 4000);
+        scrollTop: $("#box").offset().top
+    }, 4000);
+
+
+
+});
+$("#button1").click(function(){
+
+
+  $('html, body').animate({
+        scrollTop: $("#box1").offset().top
+    }, 4000);
+
+
+
+});
+$("#button2").click(function(){
+
+
+  $('html, body').animate({
+        scrollTop: $("#box2").offset().top
+    }, 4000);
 
 
 
